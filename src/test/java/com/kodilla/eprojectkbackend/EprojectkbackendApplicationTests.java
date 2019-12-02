92183d493406b8c3c970362be646dbf5fd336e53
ganzes <charczuk.arkadiusz@gmail.com>
2019-12-02 19:26:44 +0100

    initial commit

---
+++ src/test/java/com/kodilla/eprojectkbackend/EprojectkbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.eprojectkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EprojectkbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
