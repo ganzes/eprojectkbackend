a27c29580b16ac656abada021ec2c09081519434
ganzes <charczuk.arkadiusz@gmail.com>
2019-12-10 01:40:40 +0100

    added basic test/check for scheudler

--- src/main/java/com/kodilla/eprojectkbackend/configuration/CoreConfiguration.java
+++ src/main/java/com/kodilla/eprojectkbackend/configuration/CoreConfiguration.java
@@ -2,13 +2,21 @@ package com.kodilla.eprojectkbackend.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
 
+@EnableScheduling
 @Configuration
 public class CoreConfiguration {
 
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void check(){
+        System.out.println("TTTTTEEEEEESSSSSTTTTTT");
     }
 }
